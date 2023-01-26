851630ded73d28e994bf89e3b76b9991623844c5
Abhir Karande <abhir511@gmail.com>
2023-01-25 23:20:48 -0500

    working client and server

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import sys
-
-client_name = "Test Client"
+import pickle
+client_name = "Client of Abhir Karande"
 server_port = 6000
 
 # get the integer value from the command line
@@ -14,16 +14,15 @@ if client_number > 100 or client_number < 1:
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect to the server
-client_socket.connect(("", server_port))
+client_socket.connect(("127.0.0.1", server_port))
 
 # send the client's name and number to the server
-client_data = client_name + " " + str(client_number)
-client_socket.send(client_data.encode())
+client_data = pickle.dumps([client_name, client_number])
+client_socket.send(client_data)
 
 # receive data from the server
-server_data = client_socket.recv(1024).decode()
-server_name, server_number = server_data.split()
+server_name, server_number = pickle.loads(client_socket.recv(1024))
 server_number = int(server_number)
-print("Received data from server: ", server_data)
+print("Sum of values ", client_number + server_number)
 
-# print the client's name and the server's name
+client_socket.close()
--- server.py
+++ server.py
@@ -1,28 +1,28 @@
 import socket
 import random
-
-server_name = "Server of John Q. Smith"
+import pickle
+server_name = "Server of Abhir Karande"
 server_port = 6000 # choose a port number greater than 1023
 
 # create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # bind the socket to the server address and port
-server_socket.bind(("", server_port))
+server_socket.bind(("127.0.0.1", server_port))
 
 # listen for incoming connections
 server_socket.listen(1)
 
+
 while True:
     # accept a connection from a client
     client_socket, client_address = server_socket.accept()
-    print("Connected to ", client_address)
+    
     
     # receive data from the client
-    client_data = client_socket.recv(1024).decode()
-    client_name, client_number = client_data.split()
+    client_name, client_number = pickle.loads(client_socket.recv(1024))
+    print("The server of ", server_name, " is communicating with ", client_name)
     client_number = int(client_number)
-    print("Received data from client: ", client_data)
     
     if client_number > 100 or client_number < 1:
         print("Invalid client number. Terminating server.")
@@ -31,19 +31,7 @@ while True:
         
     # pick a random number between 1 and 100
     server_number = random.randint(1, 100)
-    print("Server number: ", server_number)
-    
-    # print the client's name and the server's name
-    print("Client name: ", client_name)
-    print("Server name: ", server_name)
-    
-    # compute the sum of the two numbers
-    sum_of_numbers = client_number + server_number
-    print("Sum of values: ", sum_of_numbers)
-    
-    # send the server's name and number back to the client
-    server_data = server_name + " " + str(server_number)
-    client_socket.send(server_data.encode())
-    
-    # close the client socket
+
+    server_data = pickle.dumps([server_name, server_number])    
+    client_socket.send(server_data)
     client_socket.close()
